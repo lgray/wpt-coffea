127276cb1fea3ce21edfa7cf4530732c208bff60
yongbinfeng <ybfeng94@gmail.com>
2021-09-24 02:34:04 -0500

    1st runable version to run linear extrapolation and produce templates for qcd

--- src/wpt_coffea/workflows/__init__.py
+++ src/wpt_coffea/workflows/__init__.py
@@ -1,7 +1,9 @@
 from wpt_coffea.workflows.ptw import WpTProcessor
+from wpt_coffea.workflows.ptwqcd import WpTQCDProcessor
 
 workflows = {}
 
 workflows["wpt"] = WpTProcessor
+workflows["wptqcd"] = WpTQCDProcessor
 
 __all__ = ["workflows"]
